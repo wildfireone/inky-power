1ec79f64ba88f2210f1feea6316b26c8bba8b557
Wildfireone <wildfireone@gmail.com>
2020-04-28 21:45:33 +0100

    added chart

--- texttest.py
+++ texttest.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
 import json
 import requests
+import matplotlib.pyplot as plt 
 
 inky_display = InkyWHAT("red")
 inky_display.set_border(inky_display.WHITE)
@@ -12,7 +13,9 @@ img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype(FredokaOne, 36)
 
-
+dates =[]
+power =[]
+weather =[]
 # Make a get request to get the latest position of the international space station from the opennotify api.
 response = requests.get("https://pvoutput.org/service/r2/getoutput.jsp?key=4d57028d8a05f4c6a593d97a5a5bfe48f4cb261e&sid=41079")
 # Print the status code of the response.
@@ -22,10 +25,37 @@ index =0;
 for day in data:
     index += 1
     daydata = day.split(",");
-    print("Date: {0}, Power : {1}, Weather : {2}".format(daydata[0], daydata[1], daydata[7])) 
-    
+    date  = daydata[6]+daydata[7]+"/"+daydata[4]+daydata[5]+"/"+daydata[0]+daydata[1]+daydata[2]+daydata[3]
+    dates.apppend(date)
+    power.append(daydata[1])
+    weather.apppend(daydata[7])
+    print("Date: {0}, Power : {1}, Weather : {2}".format(date, daydata[1], daydata[7])) 
     if index == daysago:
         break
+
+
+# x-coordinates of left sides of bars  
+left = [1, 2, 3, 4, 5] 
+  
+# heights of bars 
+height = power
+  
+# labels for bars 
+tick_label = dates
+  
+# plotting a bar chart 
+plt.bar(left, height, tick_label = tick_label, 
+        width = 0.8, color = ['red', 'green']) 
+  
+# naming the x-axis 
+plt.xlabel('x - axis') 
+# naming the y-axis 
+plt.ylabel('y - axis') 
+# plot title 
+plt.title('My bar chart!') 
+  
+# function to show the plot 
+plt.show() 
 
 message = "Hi Pam"
 w, h = font.getsize(message)
