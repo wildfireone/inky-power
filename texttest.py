3b377f29898d49082ffb4d5dcae879a4e7ed21a7
john <wildfireone@gmail.com>
2020-04-29 00:07:21 +0100

    adding imagine

--- texttest.py
+++ texttest.py
@@ -40,16 +40,16 @@ for day in data:
 left = [1, 2, 3, 4, 5] 
   
 # heights of bars 
-#height = power
+height = power
   
 # labels for bars 
-#tick_label = dates
+tick_label = dates
 
 # heights of bars 
-height = [10, 24, 36, 40, 5] 
+#height = [10, 24, 36, 40, 5] 
   
 # labels for bars 
-tick_label = ['one', 'two', 'three', 'four', 'five'] 
+#tick_label = ['one', 'two', 'three', 'four', 'five'] 
 
 plt.figure(figsize=(4, 3), dpi=50)
 # plotting a bar chart 
